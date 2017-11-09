ce01cbd7e561508f59e4d9dfa27ee278aabab11d
dgplock <dgplock@gmail.com>
2017-11-08 22:16:31 -0700

    Created /stc/ | stc/css/ | /templates/ | as well as serving method for the /error path for the Go server using templates

--- src/server/server.go
+++ src/server/server.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, my name is %s!", r.URL.Path[1:])
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	layoutPath := filepath.Join("templates", "layout.html")
+	targetPath := filepath.Join("templates", "errorHandling.html")
+	
+	tmpl, _ := template.ParseFiles(layoutPath, targetPath)
+
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	//Must create a fileserver to the stc directory to access css folder
+	// in stc.
+	fileServer := http.FileServer(http.Dir("stc"))
+	http.Handle("/stc/", http.StripPrefix("/stc/", fileServer))
+	
+	//Serve templates for url with matching "LetsGo"
+	http.HandleFunc("/error", serveTemplate)
 	http.ListenAndServe(":9090", nil)
 }
