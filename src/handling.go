cc7a453bd6a519f4bb643da2c8071beb7e913c00
dgplock <dgplock@gmail.com>
2017-11-27 19:12:54 -0700

    Completed handling.go 's main funciton for slide .play in errorHandling.slide.

--- src/handling.go
+++ src/handling.go
@@ -1,14 +1,23 @@
 package main
-import "fmt"
+import(
+	"errors"
+	"fmt"
+)
 
 func sqrtWithErrorReturn(input float64) (float64, error) {
-	if f < 0{
+	if input < 0{
 		return 0, errors.New("Input value is below zero.")
 	}
 	// ... Rest of the sqrt function
+
+	return 0, nil
 }
 
 func main(){
 	result, err := sqrtWithErrorReturn(-1)
-	fmt.Println("Returned error:" + err)
+	fmt.Print("Returned Value: ")
+	fmt.Print(result);
+	fmt.Print("\nReturned Error: ")
+	fmt.Print(err)
+	fmt.Print("\n")
 }
